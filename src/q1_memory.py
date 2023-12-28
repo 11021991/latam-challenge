844fd56b78483408a4d779e2f94476be467c52af
Fabian Cifuentes <cifuentes.fa@gmail.com>
2023-12-28 17:35:00 -0300

    environment

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,8 @@
+import pandas as pd
 from typing import List, Tuple
 from datetime import datetime
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    #df = pd.read_json('https://drive.google.com/file/d/1ig2ngoXFTxP5Pa8muXo02mDTFexZzsis/view?usp=sharingç')
+    #print(df.head())
+    print('fin')
